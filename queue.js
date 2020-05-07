e96ba61b74fcd95be626b3960e7e004a13642a79
daiyuanhao <948675173@qq.com>
2020-05-07 18:20:53 +0800

    queue

---
+++ queue.js
@@ -0,0 +1,32 @@
+function Queue() {
+  
+  this.items = []
+
+  Queue.prototype.enqueue = function (element) {
+    this.items.push(element)
+  }
+
+  Queue.prototype.dequeue = function (element) {
+    this.items.shift(element)
+  }
+  
+  Queue.prototype.front = function () {
+    return this.items[0]
+  }
+
+  Queue.prototype.isEmpty = function () {
+    return this.items.length == 0
+  }
+
+  Queue.prototype.size = function () {
+    return this.items.length
+  }
+
+  Queue.prototype.toString = function () {
+    var resultString = ''
+    for (var i = 0; i < this.items.length; i++){
+      resultString += this.items[i] + ' '
+    }
+    return resultString
+  }
+}
