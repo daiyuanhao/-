afa4e23fe3810deadaf319c1d9e471f6931e3c9b
daiyuanhao <948675173@qq.com>
2020-05-07 18:21:46 +0800

    dictionary

---
+++ dictionary.js
@@ -0,0 +1,38 @@
+function Dictionary(){
+
+  this.items = {}
+
+  Dictionary.prototype.set = function(key,value) {
+    this.items[key] = value
+  }
+  
+  Dictionary.prototype.has = function(key){
+    return this.items.hasOwnProperty(key)
+  }
+  
+  Dictionary.prototype.remove = function(key){
+    if(!this.has(key)) return false
+    delete this.items[key]
+    return true
+  }
+
+  Dictionary.prototype.get = function(key){
+    return this.has(key) ? this.items[key] : undefined
+  }
+  
+  Dictionary.prototype.keys = function(){
+    return Object.keys(this.items)
+  }
+
+  Dictionary.prototype.values = function(){
+    return Object.values(this.items)
+  }
+
+  Dictionary.prototype.size = function(key){
+    return Object.keys(this.items).length
+  }
+
+  Dictionary.prototype.clear = function(){
+    this.items = {}
+  }
+}
